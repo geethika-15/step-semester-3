3ae252500d1bcc9e3a975ecd18cc189a1682b237
geethika-15 <geethikaallada@gmail.com>
2026-08-29 00:50:34 +0530

    Add NumCheck program

---
+++ semester/src/main/java/org/example/NumCheck.java
@@ -0,0 +1,23 @@
+package org.example;
+
+    public class NumCheck {
+
+        static void classifyNumber(int number) {
+            if (number > 0)
+                System.out.println("Positive");
+            else if (number < 0)
+                System.out.println("Negative");
+            else
+                System.out.println("Zero");
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int number = sc.nextInt();
+            classifyNumber(number);
+
+            sc.close();
+        }
+    }
+}
