3e6e61adbc2ba464b50efb5fc13daddf5b93d8e8
geethika-15 <geethikaallada@gmail.com>
2026-08-29 00:53:03 +0530

    Add PrintNum program

---
+++ semester/src/main/java/org/example/PrintNum.java
@@ -0,0 +1,19 @@
+package org.example;
+
+    public class PrintNum {
+
+        static void printNumbersUpToN(int n) {
+            for (int i = 1; i <= n; i++)
+                System.out.print(i + " ");
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            printNumbersUpToN(n);
+
+            sc.close();
+        }
+    }
+}
