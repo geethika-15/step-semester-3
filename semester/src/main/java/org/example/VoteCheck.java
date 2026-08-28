32fca4004bf4236deac816166b757d25209013de
geethika-15 <geethikaallada@gmail.com>
2026-08-29 00:42:02 +0530

    Add VoteCheck program

---
+++ semester/src/main/java/org/example/VoteCheck.java
@@ -0,0 +1,23 @@
+package org.example;
+
+    public class VoteCheck {
+
+        static void checkVotingEligibility(int age) {
+            boolean eligible = age >= 18;
+
+            if (eligible)
+                System.out.println("Eligible to vote");
+            else
+                System.out.println("Not eligible to vote");
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int age = sc.nextInt();
+            checkVotingEligibility(age);
+
+            sc.close();
+        }
+    }
+}
